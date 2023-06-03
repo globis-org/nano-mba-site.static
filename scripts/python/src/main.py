3f7bd53f44cd9338dc3cb27520ab9cba64a9e34a
SuzukiGithub <>
2023-06-04 01:57:45 +0900

    feat: [NANOMBA-4678] add automation by cron

---
+++ scripts/python/src/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import datetime
+import requests
+import json
+from const import \
+    PATH_DIR_SRC_ROOT, \
+    PATH_FILE_ENDPOINT_SCHEDULE_JSON, \
+    EXTERNAL_API_BACKEND_SCHEDULE
+
+
+def main():
+    timestamp('[start]')
+    os.chdir(PATH_DIR_SRC_ROOT)
+
+    response = requests.get(EXTERNAL_API_BACKEND_SCHEDULE)
+    response.raise_for_status()
+    response_json = response.json()
+    print(response_json)
+    with open(PATH_FILE_ENDPOINT_SCHEDULE_JSON, 'w') as json_file:
+        s = json.dumps(response_json, indent=2, sort_keys=False, separators=(",", ": "), ensure_ascii=False)
+        json_file.write(s)
+    timestamp('[finish]')
+
+
+def timestamp(message):
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f') + '\t' + message)
+
+
+if __name__ == "__main__":
+    main()
